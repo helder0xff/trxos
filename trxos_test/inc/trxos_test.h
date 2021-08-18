9e663c4a9ed737b4e468941e2e4e98c49e27b7c7
Hélder Rodrígues <helder0xff@gmail.com>
2021-08-18 22:05:17 +0100

    FEATURE: Add test files + very simple test script (return success)

---
+++ trxos_test/inc/trxos_test.h
@@ -0,0 +1,13 @@
+/**
+ * @file	trxos_test.h
+ * @author	helder
+ * @date	2021-08-18
+ * @brief	??
+ */
+
+#ifndef TRXOS_TEST_H
+#define TRXOS_TEST_H
+
+#endif /* TRXOS_TEST_H */
+
+//*** end of file ***//
---
+++ trxos_test/test.py
@@ -0,0 +1,11 @@
+import sys
+
+PASS = 0
+FAIL = -1
+
+def main( ):
+	result = PASS
+
+	sys.exit( result )
+
+main( )
