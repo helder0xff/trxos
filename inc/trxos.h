14b81993264f891f1c4a64f9c8562c31d7e46151
Hélder Rodrígues <helder0xff@gmail.com>
2021-08-18 22:04:37 +0100

    FEATURE: Initialize source structure.

---
+++ inc/trxos.h
@@ -0,0 +1,13 @@
+/**
+ * @file	trxos.h
+ * @author	helder
+ * @date	2021-08-18
+ * @brief	??
+ */
+
+#ifndef TRXOS_H
+#define TRXOS_H
+
+#endif /* TRXOS_H */
+
+//*** end of file ***//
---
+++ src/trxos.c
@@ -0,0 +1,25 @@
+/**
+ * @file	trxos.c
+ * @author	helder
+ * @date	2021-08-18
+ * @brief	??
+ */
+
+/* Includes go here.	*/
+#include <trxos.h>
+
+/* typedefs go here.	*/
+
+/* Consts go here.	*/
+
+/* #defines go here.	*/
+
+/* static vars go here.	*/
+
+/* static function declarations go here.	*/
+
+/* non static function implementation go here.	*/
+
+/* static function implementation go here.	*/
+
+//*** end of file ***//
