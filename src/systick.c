9aabc2dfc350178e616adbfd9699d3f1294fd416
Hélder Rodrígues <helder0xff@gmail.com>
2022-09-19 22:10:03 +0100

    FIX-SYSTICK: Avoid overload of the STRELOAD value.

--- src/systick.c
+++ src/systick.c
@@ -10,7 +10,7 @@ extern uint32_t g_system_clk_Hz;
 
 void SYSTICK_init(uint32_t period_uS) {	
 	STCTRL 		= 0;                                            /* Disable. */
-	STRELOAD 	= ((g_system_clk_Hz / 1000000) * period_uS) - 1;  /* Set reload val */	
+	STRELOAD 	= (uint32_t)(((g_system_clk_Hz * 1.0 / 1000000) * period_uS) - 1);  /* Set reload val */	
 	STCURRENT 	= 0;                                            /* Reset counter. */
 	SYSPRI3 	= (SYSPRI3 & 0x00FFFFFF) | 0xE0000000;          /* Priority = 7. */
 	STCTRL 		= 0b111; 								     	/* 	Src Clk + 
